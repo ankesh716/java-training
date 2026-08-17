c80e073235929ddd4d4dfce109151168d0f32dfa
Ankesh Kumar <Ankesh.kumar.cs27@iilm.edu>
2026-08-17 11:21:44 +0530

    Time: 3 ms (20.72%), Space: 43.5 MB (14.42%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,55 +1,28 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
 
-        int i = s.length() - 1;
-        int j = t.length() - 1;
+        Stack<Character> st1 = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
 
-        while (i >= 0 || j >= 0) {
-
-            int skipS = 0;
-
-            while (i >= 0) {
-                if (s.charAt(i) == '#') {
-                    skipS++;
-                    i--;
-                } 
-                else if (skipS > 0) {
-                    skipS--;
-                    i--;
-                } 
-                else {
-                    break;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '#') {
+                if (!st1.isEmpty()) {
+                    st1.pop();
                 }
+            } else {
+                st1.push(s.charAt(i));
             }
-
-            int skipT = 0;
-
-            while (j >= 0) {
-                if (t.charAt(j) == '#') {
-                    skipT++;
-                    j--;
-                } 
-                else if (skipT > 0) {
-                    skipT--;
-                    j--;
-                } 
-                else {
-                    break;
-                }
-            }
-
-            if (i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j)) {
-                return false;
-            }
-
-            if ((i >= 0) != (j >= 0)) {
-                return false;
-            }
-
-            i--;
-            j--;
         }
 
-        return true;
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == '#') {
+                if (!st2.isEmpty()) {
+                    st2.pop();
+                }
+            } else {
+                st2.push(t.charAt(i));
+            }
+        }
+        return st1.equals(st2);
     }
 }
